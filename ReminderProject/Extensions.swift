03eadaa5b3d2d05e5e3ac2db9175c02a6108061d
Corey Green <257638@nttdata.com>
2023-05-10 15:40:59 -0500

    cleaned up add/edit reminder page

--- ReminderProject/Extensions.swift
+++ ReminderProject/Extensions.swift
@@ -7,8 +7,17 @@
 
 import Foundation
 import UIKit
+import SwiftUI
 
 extension UIView {
+    // For laying out views with code
+
+    public var width: CGFloat { return frame.size.width }
+    public var height: CGFloat { return frame.size.height }
+    public var top: CGFloat { return frame.origin.y }
+    public var bottom: CGFloat { return frame.origin.y + frame.size.height }
+    public var left: CGFloat { return frame.origin.x }
+    public var right: CGFloat { return frame.origin.x + frame.size.width }
     
     func setCellShadow() {
         self.layer.cornerRadius = 15
--- ReminderProject/ViewControllers/AddEditReminderViewController.swift
+++ ReminderProject/ViewControllers/AddEditReminderViewController.swift
@@ -5,34 +5,62 @@
 //  Created by Green, Corey on 5/9/23.
 //
 
+import Foundation
 import UIKit
+import SwiftUI
 
 class AddEditReminderViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        addSubviews()
-        view.backgroundColor = .systemBackground
-        // Do any additional setup after loading the view.
-    }
     
     lazy var datePicker: UIDatePicker = {
         let datePicker = UIDatePicker()
         datePicker.preferredDatePickerStyle = .inline
         datePicker.tintColor = .systemCyan
         datePicker.backgroundColor = .secondarySystemBackground
-//        datePicker.layer.cornerRadius = 10
+        datePicker.layer.cornerRadius = 10
+        datePicker.layer.masksToBounds = true // this allows you to change the corner radius
         return datePicker
     }()
     lazy var reminderName: UITextField = {
         let reminderName = UITextField()
+        reminderName.placeholder = "Reminder Name"
+        reminderName.layer.cornerRadius = 8.0
+        reminderName.backgroundColor = .secondarySystemBackground
+        reminderName.layer.masksToBounds = true
+        let leftView = UIView(frame: CGRect(x: 0, y: 0, width: 16, height: reminderName.frame.height)) // left padding to the textfield
+        reminderName.leftView = leftView
+        reminderName.leftViewMode = .always
         return reminderName
     }()
+    lazy var notes: UITextView = {
+        let notes = UITextView()
+        notes.layer.masksToBounds = true
+        notes.layer.cornerRadius = 8.0
+        notes.backgroundColor = .secondarySystemBackground
+        return notes
+    }()
     
-    override func viewDidLayoutSubviews() {
-        datePicker.frame = CGRect(x: 20, y: 100, width: view.frame.width - 40, height: view.frame.height/2)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addSubviews()
+        view.backgroundColor = .systemBackground
+        // Do any additional setup after loading the view.
     }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        datePicker.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            datePicker.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            datePicker.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            datePicker.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            datePicker.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        ])
+        reminderName.frame = CGRect(x: 20, y:  datePicker.top - 70, width: view.width - 40, height: 45)
+        notes.frame = CGRect(x: 20, y: datePicker.bottom + 25, width: view.width - 40, height: 120)
+    }
+
     func addSubviews() {
         view.addSubview(datePicker)
+        view.addSubview(reminderName)
+        view.addSubview(notes)
     }
 }
