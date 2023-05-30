760449dd7352ffc93b3bd7a310e28c5267aa5beb
Corey Green <257638@nttdata.com>
2023-05-30 13:03:48 -0500

    finished core data functions

--- ReminderProject/CoreData/CoreDataHelper.swift
+++ ReminderProject/CoreData/CoreDataHelper.swift
@@ -15,6 +15,7 @@ class CoreDataHelper {
     static let shareInstance = CoreDataHelper()
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
+    //func for creating and storing reminder ET in core data
     func createReminder(date: Date, title: String, notes: String, isRepeat: Int16) {
         let newReminder = ReminderEntity(context: context)
         newReminder.date =  date
@@ -22,14 +23,30 @@ class CoreDataHelper {
         newReminder.notes = notes
         newReminder.isRepeat = isRepeat
         newReminder.id = UUID()
+        do {
+            try context.save()
+        } catch {
+            print("error saving \(newReminder)")
+        }
     }
+    //func for updating reminder ET
     func updateReminder(withId id: UUID, newDate: Date, newTitle: String, newNotes: String, updatedRepeat: Int16) {
-        // TODO: Setup Update Reminder
-//        guard let reminder = CoreDataHelper.shareInstance.fetchReminder() else {
-//
-//        }
+        guard let reminder = CoreDataHelper.shareInstance.fetchReminderById(id) else {
+            print("couldn't find reminder with \(id)")
+            return
+        }
+        reminder.date = newDate
+        reminder.title = newTitle
+        reminder.notes = newNotes
+        reminder.isRepeat = updatedRepeat
         
+        do {
+            try context.save()
+        } catch {
+            print("error updating \(reminder)")
+        }
     }
+    //func to delete reminder from core data
     func deleteReminder(reminder: ReminderEntity) {
         context.delete(reminder)
         do {
@@ -38,7 +55,19 @@ class CoreDataHelper {
             print("error deleting \(reminder)")
         }
     }
-    
+    //func to assist the function for updating a Reminder by ID
+    func fetchReminderById(_ id: UUID) -> ReminderEntity? {
+        let fetchRequest: NSFetchRequest<ReminderEntity> = ReminderEntity.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "id == %@", id as CVarArg)
+        do {
+            let results = try context.fetch(fetchRequest)
+            return results.first
+        } catch let error as NSError {
+            print("couldn't find reminder, \(error.userInfo)")
+            return nil
+        }
+    }
+    //func to fetch all reminders by date and store them on the tableview
     func fetchReminders() -> [ReminderEntity] {
         var reminder: [ReminderEntity] = []
         
@@ -53,6 +82,4 @@ class CoreDataHelper {
         }
         return reminder
     }
-    
 }
-
